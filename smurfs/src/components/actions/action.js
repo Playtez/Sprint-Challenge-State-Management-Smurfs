0c0717ec3ca2997a250aa3f7a59c0194b450e883
playtez <aaron.pleitez12@gmail.com>
2019-12-13 13:34:09 -0600

    cleanup

--- smurfs/src/components/actions/action.js
+++ smurfs/src/components/actions/action.js
@@ -36,3 +36,22 @@ export const postSmurfs = newSmurf => dispatch => {
       dispatch({ type: POST_SMURF_FAILURE, payload: err.error });
     });
 };
+
+// ==== Delete ====
+
+export const DELETE_SMURF_START = "DELETE_SMURF_START";
+export const DELETE_SMURF_SUCCESS = "DELETE_SMURF_SUCCESS";
+export const DELETE_SMURF_FAILURE = "DELETE_SMURF_FAILURE";
+
+export const deleteSmurfs = id => dispatch => {
+  dispatch({ type: DELETE_SMURF_START });
+  console.log("postSmurfs here");
+  axios
+    .delete(`http://localhost:3333/smurfs/${id}`)
+    .then(res => {
+      dispatch({ type: DELETE_SMURF_SUCCESS, payload: id });
+    })
+    .catch(err => {
+      dispatch({ type: DELETE_SMURF_FAILURE, payload: err.error });
+    });
+};
